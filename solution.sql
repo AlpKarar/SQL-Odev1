e7c0f82c421859365c37984d6e7a508277945e34
Alp Karar <88462979+AlpKarar@users.noreply.github.com>
2022-03-07 14:24:08 +0300

    Update solution.sql

--- solution.sql
+++ solution.sql
@@ -1,25 +1,25 @@
-// Q1
+-- Q1
 SELECT title, description 
 FROM film
 
-// Q2
+-- Q2
 SELECT *
 FROM film
 WHERE 60 < length AND length < 75
 ORDER BY length ASC
 
-// Q3
+-- Q3
 SELECT *
 FROM film
 WHERE rental_rate = 0.99 AND (replacement_cost = 12.99 OR replacement_cost = 28.99)
 ORDER BY replacement_cost ASC
 
-// Q4
+-- Q4
 SELECT last_name
 FROM customer
 WHERE first_name = 'Mary'
 
-// Q5
+-- Q5
 SELECT *
 FROM film
 WHERE 50 >= length AND (replacement_cost != 2.99 AND replacement_cost != 4.99)
